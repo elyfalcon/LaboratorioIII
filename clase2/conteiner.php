8e4c6d222dcce29c269d42c663dd7ea4eb84ded1
elyfalcon <elyfalcon@yahoo.com>
2019-05-13 11:36:50 -0300

    falta probar

--- clase2/conteiner.php
+++ clase2/conteiner.php
@@ -9,7 +9,6 @@ class Conteiner
 	private $_capacidad;
 	private $_tamanio;
 	private $_capacidadActual;
-
 	public $ListaP=array();
 
 
@@ -56,9 +55,51 @@ class Conteiner
 
  public function AgregarProducto($Producto)
  {
- 	
- 	
+ 	$cant=count(ListaP); //numero de elementos del array
+ 	$esta=false;
+ 	if($cant==0) 
+ 		{
+ 			array_push(ListaP, $Producto);
+ 			$this->_capacidadActual=$this->_capacidadActual-($Producto->getKilos());
+
+ 		}
+ 		else
+ 			{
+ 				if($this->_capacidadActual-($Producto->getKilos()) >=0);
+ 				{
+ 					foreach ($ListaP as $prod) {
+ 					# code...
+ 						if($prod->getId() == $Producto->getId())
+ 							{
+ 								$esta=true;
+ 								$prod->setKilos($prod->getKilos() + $Producto->getKilos());
+ 								$this->_capacidadActual=$this->_capacidadActual-($Producto->getKilos());
+ 							}
+ 					}	
+ 						if($esta=false)
+ 							{
+ 								array_push(ListaP, $Producto);
+ 								$this->_capacidadActual=$this->_capacidadActual-($Producto->getKilos());
+ 							}	
+ 				}
+ 				else
+ 					{
+ 						echo "Sin espacio";
+ 					}
+
+ 			}
  }
+
+public function MostrarProductos($ListaP)
+{
+	foreach ($ListaP as $prod) {
+		# code...
+		$info= "<h1>Listado de Productos</h1>";
+ 		$info.="<br/>Id: ".$prod->getId();
+ 		$info.="<br/>Cantidad de kilos: ".$prod->getKilos();
+ 		
+	}
+}
 
 }
 
